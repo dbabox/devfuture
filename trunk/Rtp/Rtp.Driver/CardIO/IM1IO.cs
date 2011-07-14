daead9984d6328c0a2e99b8ddcec7ac131fafdbd
sgsoft@gmail.com <sgsoft@gmail.com@dd103cd0-e2f6-11de-a6a9-d5aeed48c2a6>
2011-07-14 07:16:37 +0000

    增加M1卡和UL卡读写接口

--- trunk/Rtp/Rtp.Driver/CardIO/IM1IO.cs
+++ trunk/Rtp/Rtp.Driver/CardIO/IM1IO.cs
@@ -10,5 +10,22 @@ namespace Rtp.Driver.CardIO
 {
     public interface IM1IO
     {
+        /// <summary>
+        /// 读取M1卡的1个BLOCK,即读取16字节数据；
+        /// 成功则返回 0;
+        /// </summary>
+        /// <param name="addr">起始地址</param>
+        /// <param name="rbuff">读取结果缓冲区</param>
+        /// <returns></returns>
+        int readM1Block(byte addr, byte[] rbuff);
+
+        /// <summary>
+        /// 写M1卡的1个BLOCK,即写16字节数据；
+        /// 成功则返回 0;
+        /// </summary>
+        /// <param name="addr">起始地址</param>
+        /// <param name="wbuff">写入内存缓冲区</param>
+        /// <returns></returns>
+        int writeM1Block(byte addr, byte[] wbuff);
     }
 }
--- trunk/Rtp/Rtp.Driver/CardIO/IUltralightIO.cs
+++ trunk/Rtp/Rtp.Driver/CardIO/IUltralightIO.cs
@@ -9,6 +9,22 @@ using System.Text;
 namespace Rtp.Driver.CardIO
 {
     public interface IUltralightIO
-    {
+    {   
+        /// <summary>
+        /// 读取Ultralight卡的1个Page,即4字节；
+        /// 成功则返回 0
+        /// </summary>
+        /// <param name="addr">起始地址</param>
+        /// <param name="rbuff">读取结果缓冲区</param>
+        /// <returns></returns>
+        int readULPage(byte addr,byte[] rbuff);
+
+        /// <summary>
+        /// 写Ultralight卡的1个Page，即写4字节数据；
+        /// </summary>
+        /// <param name="addr">起始地址</param>
+        /// <param name="wbuff">写入内存缓冲区</param>
+        /// <returns></returns>
+        int writeULPage(byte addr, byte[] wbuff);
     }
 }
